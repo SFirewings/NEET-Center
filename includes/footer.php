5d489b58a851f8d3cd0dfcc1e0e6190376938cf4
Sify Firewings <33573768+SFirewings@users.noreply.github.com>
2019-04-06 15:31:28 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,116 @@
+<footer class="page-footer text-center font-small mt-4 wow fadeIn">
+    <div class="pt-4">
+        <?php
+            if(!isset($_SESSION['username'])):
+            ?>
+        <a class="btn btn-outline-white" data-toggle="modal" data-target="#modalLoginForm"
+            role="button">Авторизация
+        <i class="fas fa-sign-in-alt ml-2"></i>
+        </a>
+        <a id="start" class="btn btn-outline-white" data-toggle="modal" data-target="#modalRegisterForm" role="button">Регистрация
+        <i class="fas fa-user-plus ml-2"></i>
+        </a>
+        <?php else: ?>
+        <a id="signout" class="btn btn-outline-white transition" href="social/logout.php" role="button">Выйти
+        <i class="fas fa-sign-out-alt ml-2"></i>
+        </a>
+        <?php endif; ?>
+    </div>
+    <hr class="my-4">
+    <div class="pb-4">
+        <a href="https://vk.com/neetcr" target="_blank">
+        <i class="fab fa-vk mr-3"></i>
+        </a>
+        <a href="#" target="_blank">
+        <i class="fab fa-twitter mr-3"></i>
+        </a>
+        <a href="#" target="_blank">
+        <i class="fab fa-youtube mr-3"></i>
+        </a>
+    </div>
+    <div class="footer-copyright py-3">
+        © 2019 Copyright:
+        <a href="https://vk.com/neetcr" target="_blank"> NEET Crimea  ❤️</a>
+        <p> Powered by SfireEngine </p>
+    </div>
+</footer>
+<script type="text/javascript" src="js/popper.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<script type="text/javascript">
+    new WOW().init();
+</script>
+<!-- -->
+<div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered " role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Авторизация</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form class="form-horizontal" action="" method="POST">
+                <div class="main-error alert alert-error hide"></div>
+                <?php if(isset($result)){
+                    echo $result;
+                    } ?>
+                <?php if(isset($welcome)){
+                    echo $welcome;
+                    } ?>
+                <div class="modal-body mx-3">
+                    <div class="md-form mb-5">
+                        <i class="fas fa-user prefix grey-text"></i>
+                        <input type="text" id="defaultForm-name" name="username" class="form-control validate">
+                        <label data-error="ошибка" data-success="верно" for="defaultForm-name">Логин</label>
+                    </div>
+                    <div class="md-form mb-4">
+                        <i class="fas fa-lock prefix grey-text"></i>
+                        <input type="password" name="password" id="defaultForm-pass" class="form-control validate">
+                        <label data-error="ошибка" data-success="верно" for="defaultForm-pass">Пароль</label>
+                    </div>
+                </div>
+                <div class="modal-footer d-flex justify-content-center">
+                    <button name="loginBtn" type="submit" value="login" class="btn btn-default">Отправить</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- -->
+<div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered " role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Регистрация</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body mx-3">
+                <div class="md-form mb-5">
+                    <i class="fas fa-user prefix grey-text"></i>
+                    <input type="text" id="orangeForm-name" class="form-control validate">
+                    <label data-error="ошибка" data-success="верно" for="orangeForm-name">Логин</label>
+                </div>
+                <div class="md-form mb-5">
+                    <i class="fas fa-envelope prefix grey-text"></i>
+                    <input type="email" id="orangeForm-email" class="form-control validate">
+                    <label data-error="ошибка" data-success="верно" for="orangeForm-email">Email</label>
+                </div>
+                <div class="md-form mb-4">
+                    <i class="fas fa-lock prefix grey-text"></i>
+                    <input type="password" id="orangeForm-pass" class="form-control validate">
+                    <label data-error="ошибка" data-success="верно" for="orangeForm-pass">Пароль</label>
+                </div>
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button class="btn btn-deep-orange">Регистрация</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,152 @@
+<?php
+    $page_title = 'Index';
+    include_once'config/Session.php';
+  	include_once'config/Database.php';
+  	include_once'config/Utilities.php';
+    include_once'controllers/ParseRegister.php';
+    include_once'controllers/ParseLogin.php';
+    ?>
+<!DOCTYPE html>
+<style> body { display: none; }
+      </style>
+<html lang="ru">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>NEET Crimea</title>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/mdb.min.css" rel="stylesheet">
+      <link href="css/style.min.css" rel="stylesheet">
+      <link href="css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
+      <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/jquery.mb.YTPlayer.js"></script>
+      <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+      <script type="text/javascript">
+          $(document).ready(function() {
+            	$("body").css("display", "none");
+          });
+      </script>
+      <script type="text/javascript">
+      $(document).ready(function() {
+          $("body").css("display", "none");
+
+          $("body").fadeIn(500);
+
+      	$("a.transition").click(function(event){
+      		event.preventDefault();
+      		linkLocation = this.href;
+      		$("body").fadeOut(500, redirectPage);
+      	});
+
+      	function redirectPage() {
+      		window.location = linkLocation;
+      	}
+      });
+      </script>
+      
+      <script>
+         jQuery(function(){
+              jQuery("#bgndVideo").YTPlayer();
+            });
+
+      </script>
+      <style type="text/css">
+         html,
+         body,
+         header,
+         .carousel {
+         height: 40vh;
+         }
+         @media (max-width: 740px) {
+         html,
+         body,
+         header,
+         .carousel {
+         height: 100vh;
+         }
+         }
+         @media (min-width: 800px) and (max-width: 850px) {
+         html,
+         body,
+         header,
+         .carousel {
+         height: 100vh;
+         }
+         }
+         @media (min-width: 800px) and (max-width: 850px) {
+         .navbar:not(.top-nav-collapse) {
+         background: black !important;
+         }
+         }
+      </style>
+   </head>
+   <body>
+      <header>
+         <nav class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
+            <div class="container">
+               <a class="navbar-brand transition" href="index.php">
+                 <strong>NEET Crimea</strong>
+                 </a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                 <span class="navbar-toggler-icon"></span>
+                 </button>
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                       <li class="nav-item">
+                          <a href="index.php" class="nav-link transition">Главная
+                          </a>
+                       </li>
+                       <li class="nav-item">
+                          <a class="nav-link transition" href="about.php">О нас</a>
+                       </li>
+                    </ul>
+                    <ul class="navbar-nav nav-flex-icons">
+                      <?php
+                        if(!isset($_SESSION['username'])):
+                        ?>
+                       <li class="nav-item">
+                          <a class="nav-link border border-light rounded"
+                             data-toggle="modal" data-target="#modalLoginForm">
+                          <i class="fas fa-sign-in-alt"></i>Вход
+                          </a>
+                       </li>
+                       <?php else: ?>
+                         <li class="nav-item">
+                            <a href="social/profile.php" class="nav-link border border-light rounded transition">
+                            <i class="fas fa-user"></i>Профиль
+                            </a>
+                         </li>
+                         <li class="nav-item">
+                            <a href="social/logout.php" style="right: -10px;" class="nav-link border border-light rounded transition">
+                            <i class="fas fa-sign-out-alt"></i>Выход
+                            </a>
+                         </li>
+                       <?php endif; ?>
+                    </ul>
+               </div>
+            </div>
+         </nav>
+         <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+
+            <div class="carousel-inner" role="listbox">
+               <div class="carousel-item active">
+                  <div class="view">
+                     <div id="bgndVideo" class="player" data-property="{videoURL:'http://youtu.be/OLQs_wqFR18', quality:'hd720', containment:'self',autoPlay:true,showControls:false, mute:true, startAt:0,stopAt:128, opacity:1,}"></div>
+                     <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                        <div class="text-center white-text mx-5 wow fadeIn">
+                           <h1 class="mb-3">
+                              <br>
+                              <strong>Региональный штаб Крымского филиала Домашней Гвардии</strong>
+                           </h1>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <main>
